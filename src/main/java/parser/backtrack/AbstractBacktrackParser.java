59b3b69fa2615263d915805ef0443537b2e9a309
hengxin <hfwei@nju.edu.cn>
2021-11-25 11:58:01 +0800

    AbstractBacktrackParser, BacktrackParser revised

--- src/main/java/parser/backtrack/AbstractBacktrackParser.java
+++ src/main/java/parser/backtrack/AbstractBacktrackParser.java
@@ -1,67 +1,81 @@
-package parser.backtrack; /***
+package parser.backtrack;
+
+/**
  * Excerpted from "Language Implementation Patterns",
  * published by The Pragmatic Bookshelf.
  * Copyrights apply to this code. It may not be used to create training material, 
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/tpdsl for more book information.
-***/
+ */
+
 import lexer.Lexer;
 import lexer.Token;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.ArrayList;
 
 public abstract class AbstractBacktrackParser {
-    Lexer input;           // from where do we get tokens?
-    List<Integer> markers; // stack of index markers into lookahead buffer
-    List<Token> lookahead; // dynamically-sized lookahead buffer
-    int p = 0;             // index of current lookahead token;
-                           // LT(1) returns lookahead[p]
+    private final Lexer input;
+    private final Deque<Integer> markers;
+    private final List<Token> buffer;
+    private int pos = 0;    // position in @buffer
 
     public AbstractBacktrackParser(Lexer input) {
         this.input = input;
-        markers = new ArrayList<Integer>(); // make marker stack
-        lookahead = new ArrayList<Token>(); // make lookahead buffer
-        sync(1); // prime buffer with at least 1 token
+        markers = new ArrayDeque<>();
+        buffer = new ArrayList<>();
+        sync(1);
     }
 
     public void consume() {
-        p++;
-        // have we hit end of buffer when not backtracking?
-        if ( p==lookahead.size() && !isSpeculating() ) {
-            // if so, it's an opportunity to start filling at index 0 again
-            p = 0;
-            lookahead.clear(); // size goes to 0, but retains memory
+        pos++;
+        // if we reach the end of the buffer when not backtracking?
+        if (pos == buffer.size() && !isSpeculating()) {
+            pos = 0;
+            buffer.clear();
         }
-        sync(1); // get another to replace consumed token
+        sync(1);
     }
 
-    /** Make sure we have i tokens from current position p */
-    public void sync(int i) {
-        if ( p+i-1 > (lookahead.size()-1) ) {       // out of tokens?
-            int n = (p+i-1) - (lookahead.size()-1); // get n tokens
-            fill(n);            
+    // Make sure we have i tokens from current position p
+    private void sync(int nums) {
+        // out of tokens?
+        int n = (pos + nums - 1) - (buffer.size() - 1);
+        for (int i = 1; i <= n; i++) {
+            buffer.add(input.nextToken());
         }
     }
-    public void fill(int n) { // add n tokens
-        for (int i=1; i<=n; i++) { lookahead.add(input.nextToken()); }
+
+    public Token LT(int i) {
+        sync(i);
+        return buffer.get(pos + i - 1);
     }
 
-    public Token LT(int i) { sync(i); return lookahead.get(p+i-1); }
-    public int LA(int i) { return LT(i).getType(); }
+    public int LA(int i) {
+        return LT(i).getType();
+    }
+
     public void match(int x) throws MismatchedTokenException {
-        if ( LA(1) == x ) consume();
-        else throw new MismatchedTokenException("expecting "+
-                       input.getTokenName(x)+" found "+LT(1));
+        if (LA(1) == x) {
+            consume();
+        } else {
+            throw new MismatchedTokenException("expecting "+
+                    input.getTokenName(x)+" found "+LT(1));
+        }
     }
     
-    public int mark() { markers.add(p); return p; }
-    public void release() {
-        int marker = markers.get(markers.size()-1);
-        markers.remove(markers.size()-1);
-        seek(marker);
+    public void mark() {
+        markers.push(pos);
     }
-    public void seek(int index) { p = index; }
-    public boolean isSpeculating() { return markers.size() > 0; }
+
+    public void release() {
+        pos = markers.pop();
+    }
+
+    private boolean isSpeculating() {
+        return !markers.isEmpty();
+    }
 }
--- src/main/java/parser/backtrack/BacktrackParser.java
+++ src/main/java/parser/backtrack/BacktrackParser.java
@@ -3,73 +3,93 @@ package parser.backtrack;
 import lexer.Lexer;
 import lexer.ListLexer;
 
-/***
+/**
  * Excerpted from "Language Implementation Patterns",
  * published by The Pragmatic Bookshelf.
  * Copyrights apply to this code. It may not be used to create training material, 
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/tpdsl for more book information.
-***/
+ */
+
 public class BacktrackParser extends AbstractBacktrackParser {
     public BacktrackParser(Lexer input) { super(input); }
 
-    /** stat : list EOF | assign EOF ; */
+    // stat : list EOF | assign EOF
     public void stat() throws ListRecognitionException {
-        // attempt alternative 1: list EOF
-        if ( speculate_stat_alt1() ) {
+        if (speculateStatAlt1()) {
             list(); match(Lexer.EOF_TYPE);
-        }
-        // attempt alternative 2: assign EOF
-        else if ( speculate_stat_alt2() ) {
+        } else if (speculateStatAlt2()) {
             assign(); match(Lexer.EOF_TYPE);
+        } else {
+            throw new NoViableAltException("expecting stat found "+LT(1));
         }
-        // must be an error; neither matched; LT(1) is lookahead token 1
-        else throw new NoViableAltException("expecting stat found "+LT(1));
     }
 
-    public boolean speculate_stat_alt1() {
+    private boolean speculateStatAlt1() {
         boolean success = true;
-        mark(); // mark this spot in input so we can rewind
-        try { list(); match(Lexer.EOF_TYPE); }
-        catch (ListRecognitionException e) { success = false; }
-        release(); // either way, rewind to where we were
+
+        mark();
+        try {
+            list();
+            match(Lexer.EOF_TYPE);
+        } catch (ListRecognitionException e) {
+            success = false;
+        }
+        release();
+
         return success;
     }
 
-    public boolean speculate_stat_alt2() {
+    private boolean speculateStatAlt2() {
         boolean success = true;
-        mark(); // mark this spot in input so we can rewind
-        try { assign(); match(Lexer.EOF_TYPE); }
-        catch (ListRecognitionException e) { success = false; }
-        release(); // either way, rewind to where we were
+
+        mark();
+        try {
+            assign(); match(Lexer.EOF_TYPE);
+        } catch (ListRecognitionException e) {
+            success = false;
+        }
+        release();
+
         return success;
     }
 
-    /** assign : list '=' list ; // parallel assignment */
+    // assign : list '=' list
     public void assign() throws ListRecognitionException {
-        list(); match(ListLexer.EQUALS); list();
+        list();
+        match(ListLexer.EQUALS);
+        list();
     }
 
-    /** list : '[' elements ']' ; // match bracketed list */
+    // list : '[' elements ']'
     public void list() throws ListRecognitionException {
-        match(ListLexer.LBRACK); elements(); match(ListLexer.RBRACK);
+        match(ListLexer.LBRACK);
+        elements();
+        match(ListLexer.RBRACK);
     }
 
-    /** elements : element (',' element)* ; // match comma-separated list */
+    // elements : element (',' element)*
     void elements() throws ListRecognitionException {
-        element(); while ( LA(1)== ListLexer.COMMA ) { match(ListLexer.COMMA); element(); }
+        element();
+        while (LA(1) == ListLexer.COMMA) {
+            match(ListLexer.COMMA);
+            element();
+        }
     }
 
-    /** element : name '=' NAME | NAME | list ; // assignment, name or list */
+    // element : NAME '=' NAME | NAME | list
     void element() throws ListRecognitionException {
-        if ( LA(1)==ListLexer.NAME && LA(2)==ListLexer.EQUALS) {
+        if (LA(1) == ListLexer.NAME && LA(2) == ListLexer.EQUALS) {
             match(ListLexer.NAME);
             match(ListLexer.EQUALS);
             match(ListLexer.NAME);
+        } else if (LA(1) == ListLexer.NAME) {
+            match(ListLexer.NAME);
+        } else if (LA(1)==ListLexer.LBRACK) {
+            list();
+        } else {
+            throw new NoViableAltException("expecting element found "+LT(1));
         }
-        else if ( LA(1)==ListLexer.NAME ) match(ListLexer.NAME);
-        else if ( LA(1)==ListLexer.LBRACK ) list();
-        else throw new NoViableAltException("expecting element found "+LT(1));
     }
 }
